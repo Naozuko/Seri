a4a9c32c3a3a95f02e8e3a5c52e0392fba87e0d1
Benjamin <Naozuko123@gmail.com>
2025-07-30 17:57:45 +1000

    adwa

--- mediakit/config.js
+++ mediakit/config.js
@@ -10,7 +10,7 @@ const mediaKitConfig = {
         username: "@seriixo",
         bio: "I love creating content that truly connects and builds a loyal, engaged community. With a mix of creativity, silly videos, and dedication, I will do ANYTHING to make every post go viral—including our collabs! I'd be so excited to get to know your beautiful brand and showcase it in a way my audience will genuinely love.",
         profileImage: "profile.jpg", // Place your profile image in the same directory
-        email: "seriixo.mgmt@gmail.com",
+        email: "management@seriixo.com",
         socialLinks: {
             tiktok: "https://www.tiktok.com/@seriixo",
             youtube: "https://www.youtube.com/@seriixo/shorts"
